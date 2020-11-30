1bab002689843b92ed7745fd4fcc351b1bc0a1d7
Amir Mohseni <64036222+Amir-Mohseni@users.noreply.github.com>
2020-11-30 11:41:12 +0330

    Footer and process
    FooterLink component and Process component added

---
+++ src/components/FooterLinks.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+class FooterLinks extends React.Component {
+  render() {
+    return (
+      <div className="footer-alt">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="float-left pull-none ">
+                <p className="copy-rights text-muted">
+                  {new Date().getFullYear()} © Symbit -{" "}
+                  <a href="amirmohseni.dev">Amir Mohseni</a>
+                </p>
+              </div>
+              <div className="float-right pull-none ">
+                <img src="images/payment.png" alt="payment-img" height="36" />
+              </div>
+              <div className="clearfix"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default FooterLinks;
